da4ed62509e428b4ed9fc43d16c7c6b4b820d2bd
Gaben <grodfam4@gmail.com>
2020-02-18 14:00:48 -0700

    bjk

--- Platformer/Platformer/Game1.cs
+++ Platformer/Platformer/Game1.cs
@@ -12,12 +12,23 @@ namespace Platformer
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        //player stuff
         Texture2D playerText;
         Rectangle playerRect;
-        //enemy texture and rect
+
+        //enemy stuff
         Texture2D chef1Text;
         Rectangle chef1Rect;
 
+        //variables
+        int state = 0;
+
+        //states
+        Texture2D startText;
+        Rectangle startRect;
+
+
+        KeyboardState oldKB;
 
         public Game1()
         {
@@ -33,15 +44,25 @@ namespace Platformer
         /// </summary>
         protected override void Initialize()
         {
+            //window
             this.graphics.PreferredBackBufferWidth = 1200;
             this.graphics.PreferredBackBufferHeight = 800;
             this.graphics.ApplyChanges();
 
+            //states
+            startRect = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+
+            state = 1;
+
+            //player stuff 
             playerRect = new Rectangle(100, 100, 100, 100);
+
+            //enemy stuff 
             chef1Rect = new Rectangle(200, 200, 100, 100);
+
             base.Initialize();
         }
-        
+
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -51,7 +72,13 @@ namespace Platformer
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //state stuff
+            startText = Content.Load<Texture2D>("Meme5");
+
+            //player stuff
             playerText = Content.Load<Texture2D>("pizzasteve1");
+
+            //enemy stuff 
             chef1Text = Content.Load<Texture2D>("chef1");
 
         }
@@ -74,7 +101,20 @@ namespace Platformer
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            
+
+            if (state == 1)
+            {
+                KeyboardState kb = Keyboard.GetState();
+                if (kb.IsKeyDown(Keys.Space) && oldKB.IsKeyUp(Keys.Space))
+                {
+                    state = 2;
+                }
+            }
+            if (state == 2)
+            {
+                checkKeys();               
+            }
+
 
 
             base.Update(gameTime);
@@ -89,11 +129,41 @@ namespace Platformer
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(playerText, playerRect, Color.White);
-            spriteBatch.Draw(chef1Text, chef1Rect, Color.White);
+            if (state == 1)
+            {
+                spriteBatch.Draw(startText, startRect, Color.White);
+            }
+            if (state == 2)
+            {
+                spriteBatch.Draw(playerText, playerRect, Color.White);
+                spriteBatch.Draw(chef1Text, chef1Rect, Color.White);
+            }
+
             spriteBatch.End();
-            
+
             base.Draw(gameTime);
+        }
+        private void checkKeys()
+        {
+            //Game controls
+            KeyboardState kb = Keyboard.GetState();
+            if (kb.IsKeyDown(Keys.A))
+            {
+                playerRect.X -= 5;
+            }
+            if (kb.IsKeyDown(Keys.W))
+            {
+                playerRect.Y -= 5;
+            }
+            if (kb.IsKeyDown(Keys.D))
+            {
+                playerRect.X += 5;
+            }
+            if (kb.IsKeyDown(Keys.S))
+            {
+                playerRect.Y += 5;
+            }
+            
         }
     }
 }
